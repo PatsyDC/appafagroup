717b7a2daa0def7868ff860e603b1368d958cb28
PatsyDC <patsy.delgado@tecsup.edu.pe>
2025-04-15 13:51:04 -0500

    Styles: login

--- src/app/pages/admin/components/sidebar/sidebar.component.ts
+++ src/app/pages/admin/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from 'app/core/services/user.service';
 
 interface menuSidebar{
   url: string,
@@ -21,6 +22,11 @@ export class SidebarComponent {
 
   @Output() sidebarToggled = new EventEmitter<boolean>();
 
+  constructor(
+    private userService : UserService,
+    private router: Router
+  ){}
+
   menu: menuSidebar[] = [
     { url: '/admin/inicio', title: 'Inicio', icon: "fas fa-home" },
     { url: '/admin/equiposA', title: 'Equipos', icon: "fas fa-truck-monster" }
@@ -29,6 +35,11 @@ export class SidebarComponent {
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
     this.sidebarToggled.emit(this.isSidebarOpen);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
   }
 
 
